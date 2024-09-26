d6572da9f5b70fd26faf7527cb59eb4c35a9ee1b
AdityaG <aditya.a.gaikwad13@gmail.com>
2024-09-26 12:55:45 +0530

    database details changed

--- src/main/java/com/deloitte/demo/model/Employee.java
+++ src/main/java/com/deloitte/demo/model/Employee.java
@@ -1,7 +1,17 @@
 package com.deloitte.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String firstName;
 	private double salary;
